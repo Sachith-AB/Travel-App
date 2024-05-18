91fd93aecc65b033778714d499d71ef88c400752
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2024-05-18 20:54:09 +0530

    create functionality for change state in home page

--- travel_app/lib/pages/Home_Page.dart
+++ travel_app/lib/pages/Home_Page.dart
@@ -1,5 +1,8 @@
 // ignore: file_names
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:travel_app/cubit/app_cubit.dart';
+import 'package:travel_app/cubit/app_cubit_state.dart';
 import 'package:travel_app/misc/colors.dart';
 import 'package:travel_app/widgets/app_large_text.dart';
 import 'package:travel_app/widgets/app_text.dart';
@@ -23,152 +26,158 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   Widget build(BuildContext context) {
     TabController _tabController = TabController(length: 3, vsync: this);
     return Scaffold(
-      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        Container(
-          padding: const EdgeInsets.only(top: 70, left: 20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Icon(
-                Icons.menu,
-                size: 30,
-                color: Colors.black87,
-              ),
-              Container(
-                margin: const EdgeInsets.only(right: 20),
-                width: 50,
-                height: 50,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.grey.withOpacity(0.5)),
-              )
-            ],
-          ),
-        ),
-        const SizedBox(
-          height: 40,
-        ),
-        Container(
-            margin: const EdgeInsets.only(left: 20),
-            child: AppLargeText(text: 'Discover')),
-        const SizedBox(
-          height: 30,
-        ),
-        //tabbar
-        Container(
-          child: Align(
-            alignment: Alignment.centerLeft,
-            child: TabBar(
-              labelPadding: const EdgeInsets.only(left: 20, right: 20),
-              dividerColor: Colors.white,
-              controller: _tabController,
-              labelColor: Colors.black,
-              unselectedLabelColor: Colors.grey,
-              isScrollable: true,
-              indicatorSize: TabBarIndicatorSize.label,
-              indicator:
-                  CircleTabIndicator(color: AppColors.textColor4, radius: 4),
-              tabs: const [
-                Tab(text: "Places"),
-                Tab(text: "Inspiration"),
-                Tab(text: "Emotions"),
+        body: BlocBuilder<AppCubits, CubitStates>(builder: (context, state) {
+      if (state is LoadedState) {
+        var info = state.places;
+        return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Container(
+            padding: const EdgeInsets.only(top: 70, left: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Icon(
+                  Icons.menu,
+                  size: 30,
+                  color: Colors.black87,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(right: 20),
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey.withOpacity(0.5)),
+                )
               ],
             ),
           ),
-        ),
-        Container(
-          padding: const EdgeInsets.only(left: 20),
-          height: 300,
-          width: double.maxFinite,
-          child: TabBarView(
-            controller: _tabController,
-            children: [
-              ListView.builder(
-                itemCount: 3,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  return Container(
-                    margin: const EdgeInsets.only(right: 15, top: 10),
-                    width: 200,
-                    height: 300,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.white,
-                        image: DecorationImage(
-                            image: AssetImage(
-                                'assets/${mountain.keys.elementAt(index)}'),
-                            fit: BoxFit.cover)),
-                  );
-                },
-              ),
-              const Text("There"),
-              const Text("Bye"),
-            ],
+          const SizedBox(
+            height: 40,
           ),
-        ),
-        const SizedBox(
-          height: 30,
-        ),
+          Container(
+              margin: const EdgeInsets.only(left: 20),
+              child: AppLargeText(text: 'Discover')),
+          const SizedBox(
+            height: 30,
+          ),
+          //tabbar
+          Container(
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: TabBar(
+                labelPadding: const EdgeInsets.only(left: 20, right: 20),
+                dividerColor: Colors.white,
+                controller: _tabController,
+                labelColor: Colors.black,
+                unselectedLabelColor: Colors.grey,
+                isScrollable: true,
+                indicatorSize: TabBarIndicatorSize.label,
+                indicator:
+                    CircleTabIndicator(color: AppColors.textColor4, radius: 4),
+                tabs: const [
+                  Tab(text: "Places"),
+                  Tab(text: "Inspiration"),
+                  Tab(text: "Emotions"),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(left: 20),
+            height: 300,
+            width: double.maxFinite,
+            child: TabBarView(
+              controller: _tabController,
+              children: [
+                ListView.builder(
+                  itemCount: 3,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: const EdgeInsets.only(right: 15, top: 10),
+                      width: 200,
+                      height: 300,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.white,
+                          image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/${mountain.keys.elementAt(index)}'),
+                              fit: BoxFit.cover)),
+                    );
+                  },
+                ),
+                const Text("There"),
+                const Text("Bye"),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
 
-        Container(
-          margin: const EdgeInsets.only(left: 20, right: 20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              AppLargeText(
-                text: "Explore more",
-                size: 22,
-              ),
-              AppText(
-                text: "See all",
-                color: AppColors.textColor5,
-              )
-            ],
+          Container(
+            margin: const EdgeInsets.only(left: 20, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AppLargeText(
+                  text: "Explore more",
+                  size: 22,
+                ),
+                AppText(
+                  text: "See all",
+                  color: AppColors.textColor5,
+                )
+              ],
+            ),
           ),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        Container(
-          height: 120,
-          width: double.infinity,
-          margin: const EdgeInsets.only(left: 20),
-          child: ListView.builder(
-              itemCount: 4,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (_, index) {
-                return Container(
-                  margin: const EdgeInsets.only(right: 30),
-                  child: Column(
-                    children: [
-                      Container(
-                        //margin: const EdgeInsets.only(right: 50),
-                        width: 80,
-                        height: 80,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            color: Colors.white,
-                            image: DecorationImage(
-                                image: AssetImage(
-                                    'assets/${images.keys.elementAt(index)}'),
-                                fit: BoxFit.cover)),
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        child: AppText(
-                          text: images.values.elementAt(index),
-                          color: AppColors.textColor5,
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            height: 120,
+            width: double.infinity,
+            margin: const EdgeInsets.only(left: 20),
+            child: ListView.builder(
+                itemCount: 4,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (_, index) {
+                  return Container(
+                    margin: const EdgeInsets.only(right: 30),
+                    child: Column(
+                      children: [
+                        Container(
+                          //margin: const EdgeInsets.only(right: 50),
+                          width: 80,
+                          height: 80,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.white,
+                              image: DecorationImage(
+                                  image: AssetImage(
+                                      'assets/${images.keys.elementAt(index)}'),
+                                  fit: BoxFit.cover)),
                         ),
-                      )
-                    ],
-                  ),
-                );
-              }),
-        )
-      ]),
-    );
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          child: AppText(
+                            text: images.values.elementAt(index),
+                            color: AppColors.textColor5,
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                }),
+          )
+        ]);
+      } else {
+        return Container();
+      }
+    }));
   }
 }
 
